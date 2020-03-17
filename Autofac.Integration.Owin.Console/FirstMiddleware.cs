82df847e44e71329805c60ccddf6cfade1b99b2e
Jack Zhou <J.Zhou@televic.com>
2020-03-17 15:30:02 +0800

    autofac owin integration example

---
+++ Autofac.Integration.Owin.Console/FirstMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace Autofac.Integration.Owin.Console
+{
+    public class FirstMiddleware : OwinMiddleware
+    {
+        public FirstMiddleware(OwinMiddleware next) : base(next)
+        {
+        }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            System.Console.WriteLine("Inside the 'Invoke' method of the '{0}' middleware.", GetType().Name);
+
+            await Next.Invoke(context);
+        }
+    }
+}
---
+++ Autofac.Integration.Owin.Console/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Autofac;
+
+[assembly: OwinStartup(typeof(Autofac.Integration.Owin.Console.Startup))]
+
+namespace Autofac.Integration.Owin.Console
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<FirstMiddleware>().InstancePerRequest();
+
+
+            var container = builder.Build();
+
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacLifetimeScopeInjector(container);
+
+            app.UseErrorPage();
+
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello, world.");
+            });
+        }
+    }
+}
